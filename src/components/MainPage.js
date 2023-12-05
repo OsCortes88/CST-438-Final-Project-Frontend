c503318d6e7a221e53e41619e938ef63720575ec
IssEdgarH <ehernandezheredia@csumb.edu>
2023-12-04 23:06:11 -0800

    Added for loop that calls on /videogame-info GetMapping to get more games in frontend.

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -3,10 +3,12 @@ import React, { useEffect, useState } from 'react';
 
 function MainPage() {
   const [games, setGames] = useState([]);
+  const [moreGames, setMoreGames] = useState([]);
 
   // Only changes games once upon loading.
   useEffect(() => {
     getGames();
+    getMoreGames();
   }, [])
 
   const getGames = async () => {
@@ -23,6 +25,59 @@ function MainPage() {
       console.log(res);
       // assign response to games list
       setGames(res);
+      games.forEach(function(gameId, i){
+        console.log(gameId);
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  const getMoreGames = async () => {
+    try {
+      // GET call to backend
+      const temporaryList = [];
+      for(let i = 5; i <= 8; i++) {
+        console.log('http://localhost:8080/videogame-info/' + i);
+        const response = await fetch(('http://localhost:8080/videogame-info/' + i), {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+        const res = await response.json();
+        temporaryList.push(res);
+        console.log(temporaryList);
+      }
+      for(let i = 10; i <= 14; i++) {
+        console.log('http://localhost:8080/videogame-info/' + i);
+        const response = await fetch(('http://localhost:8080/videogame-info/' + i), {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+        const res = await response.json();
+        temporaryList.push(res);
+        console.log(temporaryList);
+      }
+      for(let i = 20; i <= 25; i++) {
+        console.log('http://localhost:8080/videogame-info/' + i);
+        const response = await fetch(('http://localhost:8080/videogame-info/' + i), {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' }
+        });
+        if (!response.ok) {
+          throw new Error(`Failed to fetch data: ${response.status}`);
+        }
+        const res = await response.json();
+        temporaryList.push(res);
+        console.log(temporaryList);
+      }
+      setMoreGames(temporaryList);
+      console.log(getMoreGames);
     } catch (error) {
       console.error(error);
     }
