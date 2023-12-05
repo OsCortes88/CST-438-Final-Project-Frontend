f8dd79234f966b169b359f9e238f07cb17a1f91a
Tila Tran <artran@csumb.edu>
2023-12-04 23:34:27 -0800

    mainpage displays games in several mediums - beta

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,9 +1,29 @@
 import './MainPage.css';
 import React, { useEffect, useState } from 'react';
+import Modal from 'react-modal';
+
+const GameModal=({game, onClose}) =>{
+  return(
+    <div className="modal">
+      <div className="modal-content">
+        <span className="close" onClick={onClose}>&times;</span>
+        <h2>{game.name}</h2>
+        <img src={game.background_image} alt={game.name} />
+        <p>Release Date: {game.released}</p>
+        <p>Rating: {game.rating}</p>
+        <p>Avg Playtime: {game.playtime}</p>
+        <p>Age Rating: {game.esrb}</p>
+      </div>
+    </div>
+  );
+};
 
 function MainPage() {
   const [games, setGames] = useState([]);
   const [moreGames, setMoreGames] = useState([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [selectedGame, setSelectedGame]= useState(null);
+
 
   // Only changes games once upon loading.
   useEffect(() => {
@@ -83,17 +103,82 @@ function MainPage() {
     }
   }
 
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide + 1) % 4);
+  }
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide - 1 + 4) % 4);
+  }
+
+  const openModal = (game) =>{
+    setSelectedGame(game);
+  }
+
+  const closeModal = () =>{
+    setSelectedGame(null);
+  }
+
   return (
     <div>
-      <h1>Main Page</h1>
-      <ol>
-        {/* display data */}
-        {games.map((data) => {
+      <div class="container">
+        <nav>
+          <input type="checkbox" id="nav" class="hidden"></input>
+          <label for="nav" class="nav-btn">
+            <i></i>
+            <i></i>
+            <i></i>
+          </label>
+          <div class="logo">
+            PlaceHolder
+          </div>
+          <div class="nav-wrapper">
+            <ul>
+              <li>Home</li>
+              {/* <li>WishList</li> */}
+              <li>About</li>
+              {/* <li>LogOut</li> */}
+            </ul>
+          </div>
+        </nav>
+      </div>
+      <br></br><br></br><hr></hr><br></br><br></br>
+      <h2 style={{ textAlign: 'center' }}>Featured Games</h2>
+      <div class="slideshow-container">
+        {games.slice(0,4).map((data, index) => {
           return(
-            <li key={data.id}>{data.name}</li>
+            <div 
+              key={data.id} 
+              className={`slides ${index === currentSlide ? 'active' : '' } fade`} 
+              onClick={() => openModal(data)}
+            >
+            <img src={data.background_image} alt={`Game ${index + 1}` } />
+            <div class="text">{data.name}</div>
+          </div>
           )
         })}
-      </ol>
+        <a class="prev" onClick={prevSlide}>&#10094;</a>
+        <a class="next" onClick={nextSlide}>&#10095;</a>
+      </div>
+      <br></br><br></br><hr className="styled-hr"></hr><br></br><br></br>
+      <h3 style={{ textAlign: 'center' }}>Other Games</h3>
+      <div className="item-cards">
+        {games.slice(4,10).map((data) => (
+          <div key={data.id} className="item-card" onClick={() => openModal(data)}>
+            <img src={data.background_image} alt={data.name} />
+            <div className="card_text">{data.name}</div>
+          </div>
+        ))}
+      </div>
+      {/* Render modal */}
+      <Modal
+        isOpen={selectedGame !== null}
+        onRequestClose={closeModal}
+        className="modal-content"
+        overlayClassName="modal-overlay"
+      >
+        {selectedGame && <GameModal game={selectedGame} onClose={closeModal}/>}
+      </Modal>
     </div>
   );
 }
