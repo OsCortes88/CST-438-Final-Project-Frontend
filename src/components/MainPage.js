62adc9b56cf00ea6a6e83b0063000c03546f7a74
IssEdgarH <ehernandezheredia@csumb.edu>
2023-12-04 20:21:43 -0800

    Added GET videogames to MainPage

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,10 +1,44 @@
 import './MainPage.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function MainPage() {
+  const [games, setGames] = useState([]);
+
+  // Only changes games once upon loading.
+  useEffect(() => {
+    getGames();
+  }, [])
+
+  const getGames = async () => {
+    try {
+      // GET call to backend
+      const response = await fetch('http://localhost:8080/videogames', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' }
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.status}`);
+      }
+      const res = await response.json();
+      console.log(res);
+      // assign response to games list
+      setGames(res);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <div>
       <h1>Main Page</h1>
+      <ol>
+        {/* display data */}
+        {games.map((data) => {
+          return(
+            <li key={data.id}>{data.name}</li>
+          )
+        })}
+      </ol>
     </div>
   );
 }
