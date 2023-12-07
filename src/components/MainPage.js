85f9f8301123047a15c4569f577ab28bc5b1234b
Tila Tran <artran@csumb.edu>
2023-12-06 17:14:40 -0800

    continued modification of display games

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,5 +1,6 @@
 import './MainPage.css';
 import React, { useEffect, useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
 import Modal from 'react-modal';
 
 const GameModal=({game, onClose}) =>{
@@ -11,7 +12,7 @@ const GameModal=({game, onClose}) =>{
         <img src={game.background_image} alt={game.name} />
         <p>Release Date: {game.released}</p>
         <p>Rating: {game.rating}</p>
-        <p>Avg Playtime: {game.playtime}</p>
+        <p>Avg Playtime: {game.playtime} hours</p>
         <p>Age Rating: {game.esrb_rating}</p>
       </div>
     </div>
@@ -29,18 +30,20 @@ function MainPage() {
   const [games, setGames] = useState([]);
   const [currentSlide, setCurrentSlide] = useState(0);
   const [selectedGame, setSelectedGame]= useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const token = sessionStorage.getItem("jwt");
+  const history = useHistory();
 
   // Only changes games once upon loading.
   useEffect(() => {
-    getGames();
-  }, [])
+    getGames(currentPage);
+  }, [currentPage])
 
-  const getGames = async () => {
+  const getGames = async (page) => {
     try {
       // Get 20 games (first page)
-      const response = await fetch('http://localhost:8080/videogames/20/1', {
+      const response = await fetch(`http://localhost:8080/videogames/20/${page}`, {
         method: 'GET',
         headers: {'Authorization' : token, 'Content-Type' : 'application/json'}
       });
@@ -63,12 +66,25 @@ function MainPage() {
     setCurrentSlide((prevSlide) => (prevSlide - 1 + 4) % 4);
   }
 
+  const nextPage = () => {
+    setCurrentPage((prevPage) => prevPage + 1);
+  }
+
+  const prevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1));
+  }
+
   const openModal = (game) =>{
     setSelectedGame(game);
   }
 
   const closeModal = () =>{
     setSelectedGame(null);
+  }
+
+  const logout = () => {
+    sessionStorage.removeItem(token);
+    history.push('/');
   }
 
   return (
@@ -86,10 +102,10 @@ function MainPage() {
           </div>
           <div class="nav-wrapper">
             <ul>
-              <li>Home</li>
-              {/* <li>WishList</li> */}
-              <li>About</li>
-              {/* <li>LogOut</li> */}
+              <li><a href="/mainpage">Home</a></li>
+              <li><a>WishList</a></li>
+              <li><a>About</a></li>
+              <li><a onclick={logout}>LogOut</a></li>
             </ul>
           </div>
         </nav>
@@ -97,14 +113,10 @@ function MainPage() {
       <br></br><br></br><hr></hr><br></br><br></br>
       <h2 style={{ textAlign: 'center' }}>Featured Games</h2>
       <div class="slideshow-container">
-        {games.slice(0,4).map((data, index) => {
+        {games.slice(0,5).map((data, index) => {
           return(
-            <div 
-              key={data.id} 
-              className={`slides ${index === currentSlide ? 'active' : '' } fade`} 
-              onClick={() => openModal(data)}
-            >
-            <img src={data.background_image} alt={`Game ${index + 1}` } />
+            <div key={data.id} className={`slides ${index === currentSlide ? 'active' : '' } fade` } >
+            <img src={data.background_image} alt={`Game ${index + 1}` } onClick={() => openModal(data)}/>
             <div class="text">{data.name}</div>
           </div>
           )
@@ -115,13 +127,21 @@ function MainPage() {
       <br></br><br></br><hr className="styled-hr"></hr><br></br><br></br>
       <h3 style={{ textAlign: 'center' }}>Other Games</h3>
       <div className="item-cards">
-        {games.slice(4,10).map((data) => (
-          <div key={data.id} className="item-card" onClick={() => openModal(data)}>
-            <img src={data.background_image} alt={data.name} />
+        {games.slice(4,20).map((data) => (
+          <div key={data.id} className="item-card"> 
+            <img src={data.background_image} alt={data.name} onClick={() => openModal(data)}/>
             <div className="card_text">{data.name}</div>
           </div>
         ))}
       </div>
+        
+      <br></br><br></br>
+      <div className="pagination">
+        <button onClick={prevPage} disabled={currentPage === 1}>Previous Page</button>
+        <span>Page {currentPage}</span>
+        <button onClick={nextPage}>Next Page</button>
+      </div>
+      
       {/* Render modal */}
       <Modal
         isOpen={selectedGame !== null}
